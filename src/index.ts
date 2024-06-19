36b1e4ab2d3f1f1f186a598bf033eb1237f323b1
Marc MOREAU <moreau.marc.web@gmail.com>
2024-06-19 16:18:16 +0200

    pretty

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as database from './component/database';
-import * as componentsInterface from './component/interface/components.interface';
-import * as keyCursor from './component/key-cursor';
-import * as objectStore from './component/object-store';
-import * as storeIndex from './component/storeIndex';
-import * as transaction from './component/transaction';
-import * as valueCursor from './component/value-cursor';
-import * as databaseFactory from './database-factory';
+import * as database from './component/database'
+import * as componentsInterface from './component/interface/components.interface'
+import * as keyCursor from './component/key-cursor'
+import * as objectStore from './component/object-store'
+import * as storeIndex from './component/storeIndex'
+import * as transaction from './component/transaction'
+import * as valueCursor from './component/value-cursor'
+import * as databaseFactory from './database-factory'
 export {
-database,
-componentsInterface,
-keyCursor,
-objectStore,
-storeIndex,
-transaction,
-valueCursor,
-databaseFactory
-};
+    database,
+    componentsInterface,
+    keyCursor,
+    objectStore,
+    storeIndex,
+    transaction,
+    valueCursor,
+    databaseFactory,
+}
