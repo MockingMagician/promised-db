d0af5bc8070b09a38daa5ad2b34b2bfc4338652b
Marc MOREAU <moreau.marc.web@gmail.com>
2024-09-30 09:17:57 +0200

    clean up

--- test/browser/test/database-factory.test.ts
+++ test/browser/test/database-factory.test.ts
@@ -4,7 +4,6 @@ import {
     performInWebBrowserContext,
 } from '../../test-helpers/web-browser-context'
 import { test, expect } from '@playwright/test'
-import {randomString} from "../../test-helpers/random-string";
 
 test.describe('browser environment', () => {
     test('can create a database and record into it', async ({ page }) => {
@@ -44,43 +43,64 @@ test.describe('browser environment', () => {
     })
 
     test('async migration', async ({ page }) => {
-        const toPerform: InWebBrowserContext<unknown> = async (df: typeof DF) => {
+        const toPerform: InWebBrowserContext<unknown> = async (
+            df: typeof DF
+        ) => {
             const name = Math.random().toString(36).substring(3)
             const version = 1
 
-            let db = await df.open(name, version, [{
-                version: 1,
-                migration: async ({ db }) => {
-                    db.createObjectStore('metadata', { keyPath: 'key' })
+            let db = await df.open(name, version, [
+                {
+                    version: 1,
+                    migration: async ({ db }) => {
+                        db.createObjectStore('metadata', { keyPath: 'key' })
+                    },
                 },
-            }])
+            ])
 
             const transaction = db.transaction('metadata', 'readwrite')
 
             for (let i = 0; i < 10; i++) {
-                await transaction.objectStore('metadata').add({ key: i, value: i })
+                await transaction
+                    .objectStore('metadata')
+                    .add({ key: i, value: i })
             }
 
             await transaction.commit()
             db.close()
 
-            db = await df.open(name, version + 1, [{
-                version: 2,
-                migration: async ({ db, transaction, dbOldVersion, dbNewVersion }) => {
-                    const newStore= db.createObjectStore('new_metadata_store_with_old_data', { keyPath: 'key' })
-                    const metadataStore = transaction.objectStore('metadata')
-                    const all = await metadataStore.getAll<{key: string, value: string}, string>()
-                    for (const { key, value } of all) {
-                        await newStore.add({ key, value })
-                    }
-                    db.deleteObjectStore('metadata')
+            db = await df.open(name, version + 1, [
+                {
+                    version: 2,
+                    migration: async ({ db, transaction }) => {
+                        const newStore = db.createObjectStore(
+                            'new_metadata_store_with_old_data',
+                            { keyPath: 'key' }
+                        )
+                        const metadataStore =
+                            transaction.objectStore('metadata')
+                        const all = await metadataStore.getAll<
+                            { key: string; value: string },
+                            string
+                        >()
+                        for (const { key, value } of all) {
+                            await newStore.add({ key, value })
+                        }
+                        db.deleteObjectStore('metadata')
+                    },
                 },
-            }])
+            ])
 
             const existingStores = db.objectStoreNames
-            const dataSizeInNewMetadataSore = await db.transaction('new_metadata_store_with_old_data').objectStore('new_metadata_store_with_old_data').count()
+            const dataSizeInNewMetadataSore = await db
+                .transaction('new_metadata_store_with_old_data')
+                .objectStore('new_metadata_store_with_old_data')
+                .count()
 
-            return { existingStores, dataSizeInNewMetadataSore: dataSizeInNewMetadataSore }
+            return {
+                existingStores,
+                dataSizeInNewMetadataSore: dataSizeInNewMetadataSore,
+            }
         }
 
         const data = await performInWebBrowserContext<unknown>(page, toPerform)
